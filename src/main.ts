50e26c1f9beef938f374441b15c3b41543684461
T342 <chaircomputeroverdrive@gmail.com>
2025-06-09 17:15:07 -0700

    swap with wav??

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ window.addEventListener('click', () => {
   // Create an audio element
   // Make sure the file exists in the 'public/res' directory (or wherever your static files are served from)
   // If your build tool serves static files from 'public', use '/res/filename', otherwise try './res/filename'
-  const audioElement = new Audio('./xX_TF_CNS_TheArcade_24bit_Xx.mp3');
+  const audioElement = new Audio('./xX_TF_CNS_TheArcade_24bit_Xx.wav');
   // Loop the audio
   audioElement.loop = true;
   // Control volume
